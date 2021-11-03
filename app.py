4d1797860bd607a726de810190da77dbbb7dc413
ChronoLogic <msuttie12@hotmail.com>
2021-11-03 15:14:58 +0000

    Create method for handling POST request of new cats to server

--- app.py
+++ app.py
@@ -22,11 +22,32 @@ def get_route():
 
 @app.route("/cats", methods=["GET", "POST"])
 def get_all_cats():
-    cats = mongo.db.cats.find()
-    cats = [{**cat, "_id": str(cat["_id"])} for cat in cats]
-    if len(cats) == 0:
-        return "", 204
-    return jsonify(cats), 200
+    if request.method == "POST":
+        try:
+            response = mongo.db.cats.insert_one(request.get_json())
+            new_data = mongo.db.cats.find_one(
+                {"_id": ObjectId(response.inserted_id)}
+            )
+            new_data["_id"] = str(new_data["_id"])
+            return jsonify(new_data), 201
+
+        except (ValueError, NameError, TypeError) as err:
+            return jsonify({"error": f"{err}"}), 400
+        except:
+            return jsonify({"error": "Internal server error"}), 500
+
+    else:
+        try:        
+            cats = mongo.db.cats.find()
+            cats = [{**cat, "_id": str(cat["_id"])} for cat in cats]
+            if len(cats) == 0:
+                return "", 204
+            return jsonify(cats), 200
+
+        except ValueError as err:
+            return jsonify({"error": f"{err}"}), 400
+        except:
+            return jsonify({"error": "Internal server error"}), 500 
 
 
 @app.route("/cats/<string:_id>")
@@ -36,13 +57,13 @@ def get_cat_by_id(_id):
         if not cat:
             raise FileNotFoundError("Cat not found")
         cat["_id"] = str(cat["_id"])
-        return jsonify(cat), 200
-    except (ValueError, NameError, TypeError) as err:
+        return jsonify(cat), 200 
+    except (ValueError, NameError, TypeError, InvalidId) as err:
         return jsonify({"error": f"{err}"}), 400
     except FileNotFoundError as err:
         return jsonify({"error": f"{err}"}), 404
-    except Exception as err:
-        return jsonify({"error": "Internal server error"}), 500
+    except:
+            return jsonify({"error": "Internal server error"}), 500
 
 
 
